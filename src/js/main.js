d224060c72b4394cbd09352a60cb699266d858a6
Bomb <48523889+henryhuynh1999@users.noreply.github.com>
2023-04-19 17:12:48 +0700

    fix: fixing layout

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,33 @@ $(".banner").slick({
   autoplay: true,
   autoplaySpeed: 2000,
 });
+function activeSub() {
+  let activeSub = $(".subItem");
+  activeSub.on("click", function () {
+    $(this).toggleClass("active");
+    $(this).find(".subItem__list").toggle();
+  });
+}
+$(".header__item-search").on("click", () => {
+  $(".popup .popup__list-search").toggle();
+});
+$(".popup .popup__list-search-close-icon").on("click", () => {
+  $(".popup .popup__list-search").toggle();
+});
+$(".header__item-user").on("click", () => {
+  $(".popup .popup__list-user").toggle();
+});
+$(".popup .popup__list-like .popup__list-like-content-title-close").on(
+  "click",
+  () => {
+    $(".popup .popup__list-like").toggle();
+  }
+);
+$(".header__item-heart").on("click", () => {
+  $(".popup .popup__list-like").addClass("active");
+  $(".popup .popup__list-like").toggle();
+});
+(function () {
+  activeSub();
+  hoverImage();
+})();
